1361dca394fb362e97da9c24c209b11adae4a57a
Fadli Saad <fadlisaad@gmail.com>
2022-06-03 15:38:53 +0800

    Update verify-signature.mjs

--- verify-signature.mjs
+++ verify-signature.mjs
@@ -1,5 +1,6 @@
 import { createVerify } from 'crypto';
 import { readFileSync } from 'fs';
+import { signature, timestamp } from './generate-signature.js'
 
 /* Public key for signature verification */ 
 var key = readFileSync("myextra-public.pem", "utf8");
@@ -16,12 +17,8 @@ var reqmsg = {
 /* Request URL for Base string */
 requrl = 'https://staging.api.maybank.com/U/api/oauth2/v4/clientcred/token';
 
-/* Generate epoch timestamp */
-timestamp = "1653522922168"; // get from generate-signature
+/* Get epoch timestamp */
 console.log("X-MB-Timestamp: " + timestamp);
-
-/* Signature value get from generate signature*/ 
-var signvalue = "38NlqA3HLyY/JJC+nwVuunhUQFnGkBoiPrek6s6OnYmeDdm6QdeuvT0D71YJofMGYMcargHpUF7CQhgcY1cHU6ExDpotsW49ZZvJJ7YkfpE6r7Y8tBlVC+F/AsCTg/wbyG4JHNGA4Ac0FQwmDJ+7TiqUI2QQMG//PaKNzPZJf1L/lXJbslu+g4w1QIxO/ZR+FkUTjI1dufkg7ItylP9AYnpZN071FVKA7DNdPGXzgNVhbMfscLR9IcW7Ro8L0uWYe+GPFKbBbGIPxmGXIfxsObcvhJtC3sJGPZUdtlzg2m5rxrN11sMEqZyAg9byaDJAhDoTquCATmy7fTiHPB6cDw==";
 
 /* Signature Base String Construction  */
 signhdr = 'X-MB-Timestamp=' + timestamp + ';';
@@ -30,7 +27,7 @@ basestring = 'POST;' + encodeURIComponent(requrl) + ';' + signhdr + JSON.stringi
 /* Signature Verification */ 
 var verifier = createVerify("RSA-SHA256");
 verifier.update(basestring);
-var verifyres = verifier.verify(key, signvalue, "base64");
+var verifyres = verifier.verify(key, signature, "base64");
 
 /* Result : for Success : value is true ; Failed : false */
 console.log("signature verification result: " +verifyres);
