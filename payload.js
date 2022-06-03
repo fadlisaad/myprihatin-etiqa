103ea692f25bb5d1bcb480f4cbffac80e3184a5b
Fadli Saad <fadlisaad@gmail.com>
2022-06-03 15:51:04 +0800

    Update payload.js

--- payload.js
+++ payload.js
@@ -1,7 +1,7 @@
 import { createSign } from 'crypto';
 import { readFileSync } from "fs";
 
-var requrl,signhdr,basestring,signer,signres,clientid,Authorization;
+var requrl,signhdr,basestring,signer,signres,clientid,timestamp,Authorization;
 
 /* Private key for generating the signature */ 
 
