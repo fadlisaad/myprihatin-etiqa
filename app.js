7023e28ab2ab1167b8f8b5ecfeea27e07d17321a
Fadli Saad <fadlisaad@gmail.com>
2022-06-03 16:01:03 +0800

    Update app.js

--- app.js
+++ app.js
@@ -30,6 +30,13 @@ app.get("/payload", function (req, res) {
   })
 })
 
+// Route that receives a POST request to /sms
+app.post('/post', function (req, res) {
+  const body = req.body.Body
+  res.set('Content-Type', 'text/plain')
+  res.send('You sent: ${body}')
+})
+
 // start the server listening for requests
 app.listen(process.env.PORT || 3000, 
 	() => console.log("Server is running..."));
