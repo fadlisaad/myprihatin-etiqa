62abf0dcb55298f1a964e8d6f62b2c20431c4d03
Fadli Saad <fadlisaad@gmail.com>
2022-06-15 16:06:04 +0800

    Use unirest

--- access_token.js
+++ access_token.js
@@ -1,29 +1,25 @@
-const request = require('request');
+var unirest = require('unirest');
 import { signature, timestamp } from './generate-signature.js';
 
-var options = {
-  'method': 'POST',
-  'url': 'https://staging.api.maybank.com/U/api/oauth2/v4/clientcred/token',
-  'headers': {
+unirest('POST', 'https://staging.api.maybank.com/U/api/oauth2/v4/clientcred/token')
+  .headers({
     'X-MB-Signed-Headers': 'X-MB-Timestamp',
     'X-MB-Signature-Alg': 'RSA-SHA256',
     'X-MB-Timestamp': timestamp,
     'X-MB-Signature-Value': signature,
     'X-MB-E2E-ID': 'APIGW1646893849',
     'Content-Type': 'application/json'
-  },
-  body: JSON.stringify({
+  })
+  .send(JSON.stringify({
     "grant_type": "client_credentials",
     "scope": "motors",
     "client_id": "b41bcda226284b319a8a34e828538267",
     "client_secret": "3132a109a917494cb40b161d77a5f262"
-  })
-
-};
-
-request(options, function (error, response) {
-  if (error) throw new Error(error);
-  console.log(response.body);
-});
+  }))
+  .end(function (res) { 
+    if (res.error) throw new Error(res.error); 
+    console.log(res.raw_body);
+    const rq = res.raw_body;
+  });
 
 export const access_token = rq;
