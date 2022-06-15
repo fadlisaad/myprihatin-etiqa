0a4367f6cd10722799c2f6028ac0acfb34b710b4
Fadli Saad <fadlisaad@gmail.com>
2022-06-15 15:46:14 +0800

    Update access_token.js

--- access_token.js
+++ access_token.js
@@ -1,11 +1,9 @@
-var https = require('follow-redirects').https;
-var fs = require('fs');
+import { rq } from 'request';
 import { signature, timestamp } from './generate-signature.js'
 
 var options = {
   'method': 'POST',
-  'hostname': 'staging.api.maybank.com',
-  'path': '/U/api/oauth2/v4/clientcred/token',
+  'url': 'https://staging.api.maybank.com/U/api/oauth2/v4/clientcred/token',
   'headers': {
     'X-MB-Signed-Headers': 'X-MB-Timestamp',
     'X-MB-Signature-Alg': 'RSA-SHA256',
@@ -14,34 +12,17 @@ var options = {
     'X-MB-E2E-ID': 'APIGW1646893849',
     'Content-Type': 'application/json'
   },
-  'maxRedirects': 20
+  body: JSON.stringify({
+    "grant_type": "client_credentials",
+    "scope": "motors",
+    "client_id": "b41bcda226284b319a8a34e828538267",
+    "client_secret": "3132a109a917494cb40b161d77a5f262"
+  })
+
 };
-
-var req = https.request(options, function (res) {
-  var chunks = [];
-
-  res.on("data", function (chunk) {
-    chunks.push(chunk);
-  });
-
-  res.on("end", function (chunk) {
-    var body = Buffer.concat(chunks);
-    console.log(body.toString());
-  });
-
-  res.on("error", function (error) {
-    console.error(error);
-  });
+rq(options, function (error, response) {
+  if (error) throw new Error(error);
+  console.log(response.body);
 });
 
-var postData = JSON.stringify({
-  "grant_type": "client_credentials",
-  "scope": "motors",
-  "client_id": "b41bcda226284b319a8a34e828538267",
-  "client_secret": "3132a109a917494cb40b161d77a5f262"
-});
-
-req.write(postData);
-
-req.end();
-export { req as access_token };
+export { response.body as access_token };
