cd4febc0142b0aa15cc6315e741ba951fe2fd5d7
Fadli Saad <fadlisaad@gmail.com>
2022-06-15 16:32:39 +0800

    Declare directly in app

--- app.js
+++ app.js
@@ -33,35 +33,29 @@ app.get("/payload", function (req, res) {
   })
 })
 
-function getToken() {
-  return new Promise((resolve, reject) => {
-    uni('POST', 'https://staging.api.maybank.com/U/api/oauth2/v4/clientcred/token')
-      .headers({
-        'X-MB-Signed-Headers': 'X-MB-Timestamp',
-        'X-MB-Signature-Alg': 'RSA-SHA256',
-        'X-MB-Timestamp': timestamp,
-        'X-MB-Signature-Value': signature,
-        'X-MB-E2E-ID': 'APIGW1646893849',
-        'Content-Type': 'application/json'
-      })
-      .send(JSON.stringify({
-        "grant_type": "client_credentials",
-        "scope": "motors",
-        "client_id": "b41bcda226284b319a8a34e828538267",
-        "client_secret": "3132a109a917494cb40b161d77a5f262"
-      }))
-      .end(function (res) { 
-        if (res.error) throw new Error(res.error); 
-        res.raw_body;
-      });
-  })
-}
-
 // access token
 app.get("/access_token", function (req, res) {
   res.setHeader('Content-Type', 'application/json');
   res.send(
-    getToken()
+    uni('POST', 'https://staging.api.maybank.com/U/api/oauth2/v4/clientcred/token')
+    .headers({
+      'X-MB-Signed-Headers': 'X-MB-Timestamp',
+      'X-MB-Signature-Alg': 'RSA-SHA256',
+      'X-MB-Timestamp': timestamp,
+      'X-MB-Signature-Value': signature,
+      'X-MB-E2E-ID': 'APIGW1646893849',
+      'Content-Type': 'application/json'
+    })
+    .send(JSON.stringify({
+      "grant_type": "client_credentials",
+      "scope": "motors",
+      "client_id": "b41bcda226284b319a8a34e828538267",
+      "client_secret": "3132a109a917494cb40b161d77a5f262"
+    }))
+    .end(function (res) { 
+      if (res.error) throw new Error(res.error); 
+      res.raw_body;
+    })
   )
 })
 
