f633483c62385747a27baf9798915b86cc110140
Fadli Saad <fadlisaad@gmail.com>
2022-06-15 16:10:59 +0800

    TEst

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express()
 import { signature, timestamp } from './generate-signature.js'
 import { status } from './verify-signature.mjs';
 import { payload } from './payload.js';
-import pkg from './access_token.cjs';
+import pkg from './access_token.js';
 const { access_token } = pkg;
 
 // use the express-static middleware
